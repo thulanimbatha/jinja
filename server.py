779069f81280a3f905fc02c9630b388a139b14a1
thulanimbatha <thulanimbatha06@gmail.com>
2022-12-03 23:39:44 +0200

    added changes

--- server.py
+++ server.py
@@ -33,5 +33,13 @@ def age_name_guess(name):
 
     return render_template("guess.html", guess_name=name, guess_gender=gender, guess_age=age)
 
+# blog function that will fetch all the blogs from the URL
+@app.route('/blog')
+def blog():
+    blog_url = " https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(url=blog_url)
+    blog_posts = response.json()
+    return render_template("blog.html", posts=blog_posts)
+
 if __name__ == "__main__":
     app.run(debug=True)
